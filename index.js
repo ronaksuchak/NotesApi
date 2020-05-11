576fa6d3178b1e5aac1c864797214c5528463f18
ronaksuchak <ronaksuchak16@hotmail.com>
2020-05-11 16:57:35 +0530

    removed open shift config

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ mongoose.Promise = global.Promise;
 //setup middeleware and routes
 app.use(bodyparser.json());
 app.use('/api',require('./routes/api'));
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
 
 //start server
 app.listen(4000, () => console.log("Server Started on port 4000"));
